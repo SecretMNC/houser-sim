30b0150823ceefda21135c8529294f40ae53e8aa
Ana Ortiz <ana.ortiz.y.b.7@gmail.com>
2017-10-20 14:57:56 -0600

    added missing nav to step 2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,11 @@ import React from 'react';
 
 export default function Navbar(){
     return (
-        <nav></nav>
+        <nav className='navBar'>
+            <div className='nav_logo'></div>
+            <div>HOUSER</div>
+            <div>Dashboard</div>
+            <div></div>
+        </nav>
     )
 }
--- src/components/Step2.js
+++ src/components/Step2.js
@@ -1,12 +1,16 @@
 import React ,{Component} from 'react';
+import NavBar from './NavBar';
 import {Link } from 'react-router-dom';
 
 export default class Step2 extends Component{
     render(){
         return (
+            <div>
+                <NavBar/>
             <Link to = '/step3'><h2>
                 this is step 2
             </h2></Link>
+            </div>
         )
     }
 }
