2e6dfd34f059743463a83737f781abc02d54599f
chris-yi <kri5to.yi@gmail.com>
2017-10-20 16:44:24 -0600

    finished login endpoint

--- server/controllers/auth_controllers.js
+++ server/controllers/auth_controllers.js
@@ -1,30 +1,41 @@
 module.exports = {
-    login: (req, res, next) => {
-
-        const { session } = req;
-        const {username, password } = req.body;
-        const user = users.find( user => user.username === username && user.password === password );
-        
-        if (user) {
-            session.user.username = user.username;
-            res.status(200).send(session.user); 
+  login: (req, res, next) => {
+    const { session } = req;
+    const { username, password } = req.body;
+    let user;
+    let found = false;
+    const dbInstance = req.app.get("db");
+    dbInstance
+      .get_users()
+      .then(users => {
+        users.map(item => {
+          if (username == item.username && password == item.password) {
+            found = true;
+            user = item;
+          }
+        });
+        console.log(user)
+        if (found) {
+          session.user = user;
+          res.status(200).send(String(session.user.user_id));
         } else {
-            res.status(500).send('Unauthorized')
+          res.status(403).send("Unauthorized");
         }
-    },
-    register: (req, res, next) => {
-        const dbInstance = req.app.get('db');
-            dbInstance.create_user([req.body.username, req.body.password])
-               .then( () => {
-                   req.session.user.username = req.body.username;
-                   res.status(200).send() 
-               })
-               .catch( () => res.status(500).send() );
-
-    },
-    logout:(req, res, next) => {
-       session.destroy();
-       res.status(200).send(req.session)
-    }
-
-}
+      })
+      .catch(err => console.log(err));
+  },
+  register: (req, res, next) => {
+    const dbInstance = req.app.get("db");
+    dbInstance
+      .create_user([req.body.username, req.body.password])
+      .then(() => {
+        req.session.user.username = req.body.username;
+        res.status(200).send();
+      })
+      .catch(() => res.status(500).send());
+  },
+  logout: (req, res, next) => {
+    session.destroy();
+    res.status(200).send(req.session);
+  }
+};
