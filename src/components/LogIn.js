56a864b56ce332cabd0f83a4a5e88aa2e77dd791
Ana Ortiz <ana.ortiz.y.b.7@gmail.com>
2017-10-20 12:09:19 -0600

    working on the log in file

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,11 +1,33 @@
-import React ,{Component} from 'react';
-import {Link} from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
-export default class Login extends Component{
-    render(){
+export default class Login extends Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+    render() {
         return (
-          <Link to ='/dashboard'>  <h2>this is the login page</h2></Link>
+            <div className='logIn_body'>
+                <div className='logIn_main'>
+                    <div>
+                        <h2>Username:</h2>
+                        <input />
+                    </div>
+                    <div>
+                    <h2>Password:</h2>
+                    <input />
+                    </div>
+                    <div>
+                    <Link to='/dashboard'>  <button>Login</button></Link>
+                    <Link to='/dashboard'><button>Register</button></Link>
+                    </div>
+
+                </div>
+            </div>
         )
     }
-    
+
 }
