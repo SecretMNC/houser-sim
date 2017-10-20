8ac47a179f7a3c286f0d5a198d95ae4b38f1b8fb
Ana Ortiz <ana.ortiz.y.b.7@gmail.com>
2017-10-20 16:03:42 -0600

    updated dashboard comp

--- src/components/DashBorad.js
+++ src/components/DashBorad.js
@@ -1,15 +1,16 @@
-import React ,{Component} from 'react';
+import React, { Component } from 'react';
 import NavBar from './NavBar';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-export default class DashBoard extends Component{
-    render(){
+export default class DashBoard extends Component {
+    render() {
         return (
             <div>
-         <NavBar/>
-           <Link to='/step1'> <h2>this is the dashboard page </h2></Link>
-
-           </div>
+                <NavBar />
+                <div className='dashboard_body'>
+                    <Link to='/step1'> <button className='add_new_prop'>Add New Proterty </button></Link>
+                </div>
+            </div>
         )
     }
 }
