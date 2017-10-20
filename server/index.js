957da97bc24d0f41612baff8633320dd9d73c6ee
chris-yi <kri5to.yi@gmail.com>
2017-10-20 15:00:02 -0600

    created endpoints

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
+require("dotenv").config()
+
 const express=require('express'),
       bodyParser=require('body-parser'),
       massive=require('massive'),
       session=require('express-session'),
       cors = require('cors'),
-      port=8080;
+      port=8080,
+      pc=require("./controllers/properties_controller")
 
-require("dotenv").config()
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+// app.use(middleware.isAuthed);
+// app.use(session({
+//     secret: 'ksdhfjklsdhfkldsfksdfkklfsfwewefvinivne',
+//     resave: false,
+//     saveUninitialized: true
+// }))
+
+
 
 massive(process.env.CONNECTION_STRING)
     .then(db => {
@@ -23,6 +33,32 @@ app.get("/api/test", function(req, res) {
     })
 )})
 
+
+// app.get("/api/:shelf", shelfcontrol.getBins)
+
+// app.get("/api/:shelf/:bin", shelfcontrol.getBin)
+
+// app.put("/api/:shelf/:bin", shelfcontrol.updateBin)
+
+// app.delete("/api/:shelf/:bin", shelfcontrol.deleteBin)
+
+// app.post("/api/:shelf/:bin", shelfcontrol.createBin)
+
+
+///////// AUTH ENDPOINTS //////////
+
+// app.post()
+// app.post()
+// app.post()
+
+
+
+///////// PROP ENDPOINTS //////////
+
+app.post("/api/properties", pc.create);
+app.get("/api/properties", pc.getAll);
+app.delete("/api/properties/:id", pc.delete);
+// app.get("/api/properties/filter", pc.filter)
 
 
 
