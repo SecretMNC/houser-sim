4c34a7405d7d74447767660775d8386aaf994e66
Ana Ortiz <ana.ortiz.y.b.7@gmail.com>
2017-10-20 15:39:07 -0600

    finished navBar minus logo image

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,9 +5,9 @@ import {Link } from 'react-router-dom';
 export default function Navbar() {
     return (
         <nav className='navBar'>
-            <img url='./assets/auth_logo.png' alt='houser logo' />
-            <h1>HOUSER</h1>
-            <h2>Dashboard</h2>
+            <img url='./assets/auth_logo.png' alt='houser logo' className='nav_logo'/>
+            <h1 className='houser'>Houser</h1>
+            <h2 className='dashboard'>Dashboard</h2>
         <Link to= '/'><div className='logOut'>Logout</div></Link>
         </nav>
     )
